8c41869fa4d0c661ca95143dc07d8b19adfe236c
Aidan Gerber <agentaidan@gmail.com>
2021-09-02 23:56:27 -0500

    add matplotlib

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import json
+import pandas as pd
+import matplotlib.pyplot as plt
 
 search_results = []
-for i in range(1,10):
+for i in range(1, 10):
     with open(f'searchResults{i}.json', 'r') as myfile:
         data = myfile.read()
     results = json.loads(data)
     search_results.append(results['data'])
 
 search_results = [item for subl in search_results for item in subl]
+
+slim_search_results = [
+    {
+        'subject': course['subject'],
+        'courseTitle': course['courseTitle'],
+        'maximumEnrollment': course['maximumEnrollment'],
+        'enrollment': course['enrollment'],
+    }
+    for course in search_results
+]
+df = pd.DataFrame(slim_search_results, columns=['subject', 'courseTitle', 'maximumEnrollment', 'enrollment'])
+plt.hist(df['enrollment'], bins=50, log=True)
+plt.title('Histogram of class enrollment at Rice on a log scale')
+plt.show()
+
 total_enrollment = 0
-total_courses = len(search_results)
+total_courses = len(slim_search_results)
 total_enrollment_greater_50 = 0
 total_course_greater_50 = 0
 total_course_greater_1 = 0
 courses_greater_50 = []
-for course in search_results:
+for course in slim_search_results:
     total_enrollment += course['enrollment']
     if course['enrollment'] > 1:
         total_course_greater_1 += 1
@@ -24,6 +41,7 @@ for course in search_results:
         total_course_greater_50 += 1
 
 print(f'Total Courses: {total_courses}')
-print(f'Percent of classes greater than 50 out of all classes, including those with 1 student enrolled: {total_course_greater_50 / total_courses}')
+print(
+    f'Percent of classes greater than 50 out of all classes, including those with 1 student enrolled: {total_course_greater_50 / total_courses}')
 print(f'Percent of classes greater than 50: {total_course_greater_50 / total_course_greater_1}')
 print(f'Percent of classes greater than 50 weighted for class size: {total_enrollment_greater_50 / total_enrollment}')
